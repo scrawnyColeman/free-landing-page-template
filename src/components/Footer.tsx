901b0f9d3c9e9deb963eda9dfd9be756781006d7
scrawnyColeman <thomashutchinson450@gmail.com>
2023-01-09 23:07:08 +0000

    added my link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { FaFacebook, FaTiktok, FaInstagram } from "react-icons/fa";
 
 const Footer = () => (
-  <footer>
-    <div className="container mx-auto flex justify-center space-x-8 py-12">
+  <footer className="container mx-auto py-6">
+    <div className="mb-12 flex justify-center space-x-8">
       <a
         rel="noopener noreferrer"
         href="https://instagram.com"
@@ -16,6 +16,14 @@ const Footer = () => (
       </a>
       <a rel="noopener noreferrer" href="https://tiktok.com" title="Tiktok">
         <FaTiktok className="h-8 w-8 hover:text-lime-200" />
+      </a>
+    </div>
+    <div className="flex w-full justify-center">
+      <a
+        href="https://thomashutchinsonwebdev.vercel.app"
+        className="light-gradient-text mx-auto text-center text-lg"
+      >
+        Get your own site
       </a>
     </div>
   </footer>
