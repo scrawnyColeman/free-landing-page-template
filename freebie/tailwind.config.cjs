f8507b2d8e005370468c56c7896a16a3412c1b59
Scrawny Coleman <43850448+scrawnyColeman@users.noreply.github.com>
2023-01-22 19:45:50 +0000

    Update tailwind.config.cjs

--- freebie/tailwind.config.cjs
+++ freebie/tailwind.config.cjs
@@ -6,12 +6,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
-        nunito: ["Nunito", ...defaultTheme.fontFamily.sans],
-        sriracha: ["Sriracha", ...defaultTheme.fontFamily.sans],
-        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
-        bo: ["Black Ops One", ...defaultTheme.fontFamily.sans],
-        comfortaa: ["Comfortaa", ...defaultTheme.fontFamily.sans],      },
+        inter: ["Inter"],
+        pj: ["Plus Jakarta Sans"],
+      },
     },
   },
   plugins: [],
